02b01e5de2b631450372aeaf46f76d7198563998
Sadiksha <sadiksha.chitrakar101@gmail.com>
2022-11-11 17:03:31 +0545

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,38 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 import 'package:workout_tracker/constants/navigation.dart';
 import 'package:workout_tracker/constants/theme.dart';
 import 'package:workout_tracker/screens/home_screen.dart';
 import 'package:workout_tracker/screens/login_screen.dart';
 import 'package:workout_tracker/screens/overview_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+//     setWindowMinSize(const Size(700, 500));
+//     // setWindowMaxSize(Size.infinite);
+//   }
+//   runApp(const MyApp());
+// }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(800, 600),
+    center: true,
+    backgroundColor: Colors.transparent,
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
